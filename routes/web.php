7fb7646b70ce522d35fa6ce70d16e04633a153b9
Viet Ha <smalldream.tvn@gmail.com>
2020-06-13 00:33:48 +0700

    Remove code.

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/quiz/show', 'QuizController@show');
-Route::get('/quiz/add-quest', 'QuizController@addQuest');
